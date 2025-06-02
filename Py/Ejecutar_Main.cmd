79d17ba23954330800fb2d12bb2312f741ccf8c7
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-06-02 13:09:28 -0500

    Feat: Add script to launch Chrome and execute Playwright main script

---
+++ Py/Ejecutar_Main.cmd
@@ -0,0 +1,9 @@
+@echo off
+set "VENV_PYTHON=C:\Users\sebax\Desktop\Universidad\Proyectos_aleatorios\Extension_Skip_Ad_Chrome\Py\EnvVirtual\Scripts\python.exe"
+set "SCRIPT_PATH=C:\Users\sebax\Desktop\Universidad\Proyectos_aleatorios\Extension_Skip_Ad_Chrome\Py\main.py"
+
+title Iniciando Chrome y Script de Playwright
+
+"%VENV_PYTHON%" "%SCRIPT_PATH%"
+
+pause
